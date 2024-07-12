d761217bb3c7e134d0028f89493d67bd9125b7df
alkhemeia <alkhemeia.de@gmail.com>
2024-07-12 04:08:06 +0200

    Added language files

---
+++ language/de.php
@@ -0,0 +1,163 @@
+<?php
+$LANG_Avatar = 'Avatar';
+$LANG_Hitpoints = 'Lebenspunkte';
+$LANG_Strength = 'Stärke';
+$LANG_Toughness = 'Zähigkeit';
+$LANG_Speed = 'Geschwindigkeit';
+$LANG_Energy = 'Energie';
+$LANG_Attack = 'Angreifen';
+$LANG_Talk = 'Sprechen';
+$LANG_Refresh = 'Aktualisieren';
+$LANG_Notifications = 'Benachrichtigungen';
+$LANG_Login = 'Einloggen';
+$LANG_Back = 'Zurück';
+$LANG_and = 'und';
+$LANG_received = 'erhalten';
+$LANG_Drink = 'Trinken';
+$LANG_Info = 'Info';
+$LANG_Success = 'Erfolg';
+$LANG_Failed = 'Fehlgeschlagen';
+$LANG_Error = 'Fehler';
+$LANG_changed = 'geändert';
+$LANG_low = 'niedrig';
+$LANG_Customize = 'Unterkunft anpassen';
+$LANG_Save = 'Speichern';
+$LANG_Saved = 'Gespeichert';
+$LANG_Maximum = 'Maximal';
+$LANG_Appearance = 'Erscheinungsbild';
+$LANG_Wallpaper = 'Tapete';
+$LANG_Designation = 'Bezeichnung';
+$LANG_Close = 'Schließen';
+
+$LANG_Profile = 'Profil';
+$LANG_Skills = 'Fähigkeiten';
+$LANG_Equipment = 'Ausrüstung';
+$LANG_Inventory = 'Inventar';
+$LANG_Shopping_Street = 'Einkaufsstraße';
+$LANG_Bank = 'Bank';
+$LANG_Loan = "Kredit";
+$LANG_Residential_Area = 'Wohnviertel';
+$LANG_Hotel = 'Hotel';
+$LANG_Missions = 'Missionen';
+$LANG_Hunt = 'Jagen';
+$LANG_Ranking_List = 'Rangliste';
+$LANG_Conversations = 'Gespräche';
+$LANG_Master = 'Erschaffer';
+$LANG_Children = 'Kinder';
+$LANG_Ghouls = 'Ghule';
+
+$LANG_Years = 'Jahre';
+$LANG_Months = 'Monate';
+$LANG_Days = 'Tage';
+
+$LANG_Body = 'Körper';
+$LANG_Hair = 'Haare';
+$LANG_Beard = 'Bart';
+$LANG_Torso = 'Oberkörper';
+$LANG_Legs = 'Beine';
+$LANG_Hands = 'Hände';
+$LANG_Feet = 'Füße';
+$LANG_Body_shape = 'Körperform';
+$LANG_Shape_A = 'Form A';
+$LANG_Shape_B = 'Form B';
+
+$LANG_PayPal_Invalid_Input_Please_check_the_amount = "Ungültige Eingabe. Bitte überprüfen Sie den Betrag.";
+$LANG_PayPal_Thank_you_for_your_purchase = "Vielen Dank für deinen Kauf!";
+$LANG_PayPal_Thank_you_for_your_donation = "Vielen Dank für deine Spende!";
+
+$LANG_Edit_appearance = 'Aussehen bearbeiten';
+$LANG_characters_allowed = 'Zeichen erlaubt';
+$LANG_Invalid_image_URL = 'Ungültige Bild URL';
+
+$LANG_You_drink = 'Du trinkst';
+$LANG_Unknown_Skill = 'Unbekannte Fähigkeit';
+$LANG_Unknown_Ghoul = 'Unbekannter Ghoul';
+
+$LANG_Enter_your_name = 'Gib deinen Namen ein:';
+$LANG_Click_here_to_upload_avatar = 'Hier klicken um Avatar hochzuladen';
+$LANG_Click_here_to_upload_property = 'Hier klicken um Unterschlupf hochzuladen';
+
+$LANG_has_been_added_to_your_inventory = 'wurde deinem Inventar hinzugefügt!';
+$LANG_taken_off = 'abgelegt';
+$LANG_You_have_taken_off_a_piece_of_equipment = 'Du hast ein Ausrüstungsteil abgelegt.';
+
+$LANG_Back_to_City = 'Zurück zur Stadt';
+$LANG_Back_to_Residential_Area = 'Zurück zum Wohnviertel';
+
+$LANG_Your_name_cannot_be_longer_than_1 = 'Dein Name darf maximal';
+$LANG_Your_name_cannot_be_longer_than_2 = 'Zeichen lang sein.';
+
+$LANG_Name_changed_to_1 = 'Dein Name ist jetzt:';
+$LANG_Name_changed_to_2 = '.';
+
+$LANG_You_trained_hard = 'Du hast hart trainiert';
+
+$LANG_Talk_to_people_passing_by = 'Sprich mit den vorbeikommenden Passanten...';
+
+$LANG_BANK_WELCOME = "Willkommen in der City Bank. Zahle dein Geld kostenlos ein oder aus, um es zu schützen und erhalten dabei tägliche Zinsen auf deine Ersparnisse.";
+$LANG_BANK_INTEREST_INFO_1 = "Die Bank zahlt dir pro Tag";
+$LANG_BANK_INTEREST_INFO_2 = "% auf dein Guthaben, bis du die Obergrenze von";
+$LANG_BANK_INTEREST_INFO_3 = "erreicht hast.";
+$LANG_BANK_LOAN_INTRO = "Guten Tag! Brauchen Sie finanzielle Unterstützung für Ihre Pläne? Wir bieten attraktive Kredite zu günstigen Konditionen.";
+$LANG_BANK_LOAN_INFO_1 = "Mit unserem Kredit können Sie Ihre Träume verwirklichen, sei es ein neues Zuhause, ein Geschäft oder eine andere Investition. Unsere flexiblen Rückzahlungsoptionen und niedrigen Zinsen machen es Ihnen leicht, Ihre Ziele zu erreichen. Vertrauen Sie auf unsere Erfahrung und lassen Sie uns gemeinsam den besten Weg für Ihre finanzielle Zukunft finden. Mit Ihrem aktuellen Score können wir Ihnen einen Kredit bis zu";
+$LANG_BANK_LOAN_INFO_2 = "anbieten, im gegenzug Zahlen Sie";
+$LANG_BANK_LOAN_INFO_3 = "Zinsen auf Ihre Restschulden pro Tag. Sie erhalten in der Zeit in der Sie Schulden haben keine Zinsen auf Ihr Guthaben.";
+$LANG_BANK_DEPT_INFO_1 = "Du hast";
+$LANG_BANK_DEPT_INFO_2 = "Schulden bei der Bank. Du zahlst dadurch gerade Zinsen von";
+$LANG_BANK_DEPT_INFO_3 = "pro Tag.";
+
+$LANG_Your_Property = 'Deine Unterkunft';
+$LANG_HOTEL_RENT_INTRO = 'Willkommen im Hotel! Möchten Sie ein Zimmer mieten?';
+$LANG_HOTEL_RENT_INFO = 'Der Preis für ein Zimmer beträgt';
+$LANG_Rent_Hotel_Room = 'Hotelzimmer mieten';
+$LANG_Rent_Now = 'Für 24 Stunden mieten';
+$LANG_You_rented_a_hotel_room = 'Du hast ein Hotelzimmer gemietet';
+$LANG_Your_Hotel_Room = 'Dein Hotelzimmer';
+$LANG_Property_of = 'Unterkunft von';
+$LANG_You_are_save = 'Du bist in Sicherheit';
+$LANG_Room_No = 'Zimmer Nr.';
+$LANG_Occupied = 'Belegt';
+$LANG_Other_rooms_in_hotel = 'Weitere Zimmer im Hotel';
+$LANG_HOUSING_MARKLER_INTRO = 'Hey, Sie da! Suchen Sie nach einem neuen Zuhause? Ich habe ein exklusives Angebot!';
+$LANG_HOUSING_MARKLER_SALES_PITCH_1 = 'Dieses Haus ist nicht nur ein Gebäude, es ist ein sicherer Hafen in dieser unruhigen Stadt. Mit seiner robusten Bauweise und der strategischen Lage bietet es Ihnen Schutz und Komfort. Stellen Sie sich vor, wie Sie hier in Ruhe Ihre Pläne schmieden können. Ein solches Angebot finden Sie kein zweites Mal. Für nur';
+$LANG_HOUSING_MARKLER_SALES_PITCH_2 = 'gehört es Ihnen. Was sagen Sie?';
+$LANG_HOUSING_MARKLER_BUY_HOUSE = 'Kaufe das Haus für';
+$LANG_Buy_house = "Haus kaufen";
+
+$LANG_HOUSING_LOCKED_DOOR = 'Du stehst vor einer verschlossenen Tür, hier gibt es kein hindurchkommen.';
+$LANG_Talk_to_visitors_to_your_property = 'Sprich mit den Besuchern deiner Unterkunft';
+$LANG_Insert_image_URL = 'Bild URL einfügen';
+$LANG_This_is_not_a_valid_URL = 'Das ist keine gültige URL.';
+$LANG_Wallpaper_replaced = 'Tapete ausgetauscht.';
+$LANG_Property_name_changed = 'Name der Unterkunft geändert.';
+$LANG_Insert_label = 'Bezeichnung einfügen';
+$LANG_Change_the_appearance_of_your_property = 'Ändere das Erscheinungsbild deines Unterschlupfs.';
+$LANG_Change_the_designation_that_appears_for_your_property = 'Ändere die Bezeichnung die für deinen Unterschlupf angezeigt wird.';
+$LANG_Add_wallpaper_to_your_hideout_by_saving_a_link_to_an_image_here = 'Füge deinem Unterschlupf eine Tapete hinzu indem du den Link zu einem Bild hier speicherst.';
+
+$LANG_has_been_added_to_your_Ghouls = 'wurde deinen Ghoulen hinzugefügt.';
+$LANG_You_have_reached_the_maximum_number_of_1 = 'Du hast die maximale Anzahl an';
+$LANG_You_have_reached_the_maximum_number_of_2 = 'erreicht.';
+
+$LANG_Roam_the_city_have_adventures_and_discover_places = 'Durchstreife die Stadt, erlebe Abenteuer und entdecke neue Orte!';
+
+$LANG_Key_received = 'Schlüssel erhalten!';
+$LANG_Key_taken_back = 'Schlüssel zurückgenommen!';
+$LANG_gave_you_a_key_to_property = ' hat dir einen Schlüssel zu seiner/ihrer Unterkunft gegeben.';
+$LANG_has_taken_back_the_key_to_accomodation = ' hat den Schlüssel zu seiner/ihrer Unterkunft zurückgenommen.';
+$LANG_You_gave_a_key_to_your_property_1 = 'Du gibst ';
+$LANG_You_gave_a_key_to_your_property_2 = ' einen Schlüssel zu deiner Unterkunft.';
+$LANG_You_took_the_key_to_your_property_from_1 = 'Du hast den Schlüssel zu deiner Unterkunft von ';
+$LANG_You_took_the_key_to_your_property_from_2 = ' zurück erhalten.';
+
+$LANG_senses_are_particularly_heightened = '\'s Sinne sind besonders geschärft.';
+
+$LANG_Your_skill_has_increased_1 = 'Deine Fähigkeit ';
+$LANG_Your_skill_has_increased_2 = ' ist gestiegen.';
+
+$LANG_Wishing_Well = 'Wunschbrunnen';
+$LANG_Throw_a_donation = 'Wirf eine Spende in den Brunnen und hilf uns, die Stadt zu verbessern!';
+
+$LANG_What_a_bargain = 'Was für ein Schnäppchen!';
+$LANG_You_dont_have_enough = 'Du hast nicht genug';
+$LANG_You_bought_a_house = 'Du hast ein Haus gekauft!';
---
+++ language/en.php
@@ -0,0 +1,163 @@
+<?php
+$LANG_Avatar = 'Avatar';
+$LANG_Hitpoints = 'Hitpoints';
+$LANG_Strength = 'Strength';
+$LANG_Toughness = 'Toughness';
+$LANG_Speed = 'Speed';
+$LANG_Energy = 'Energy';
+$LANG_Attack = 'Attack';
+$LANG_Talk = 'Talk';
+$LANG_Refresh = 'Refresh';
+$LANG_Notifications = 'Notifications';
+$LANG_Login = 'Login';
+$LANG_Back = 'Back';
+$LANG_and = 'and';
+$LANG_received = 'received';
+$LANG_Drink = 'Drink';
+$LANG_Info = 'Info';
+$LANG_Success = 'Success';
+$LANG_Failed = 'Failed';
+$LANG_Error = 'Fehler';
+$LANG_changed = 'changed';
+$LANG_low = 'low';
+$LANG_Customize = 'Customize';
+$LANG_Save = 'Save';
+$LANG_Saved = 'Saved';
+$LANG_Maximum = 'Maximum';
+$LANG_Appearance = 'Appearance';
+$LANG_Wallpaper = 'Wallpaper';
+$LANG_Designation = 'Designation';
+$LANG_Close = 'Close';
+
+$LANG_Profile = 'Profile';
+$LANG_Skills = 'Skills';
+$LANG_Equipment = 'Equipment';
+$LANG_Inventory = 'Inventory';
+$LANG_Shopping_Street = 'Shopping Street';
+$LANG_Bank = 'Bank';
+$LANG_Loan = "Loan";
+$LANG_Residential_Area = 'Residential Area';
+$LANG_Hotel = 'Hotel';
+$LANG_Missions = 'Missions';
+$LANG_Hunt = 'Hunt';
+$LANG_Ranking_List = 'Ranking List';
+$LANG_Conversations = 'Conversations';
+$LANG_Master = 'Master';
+$LANG_Children = 'Children';
+$LANG_Ghouls = 'Ghouls';
+
+$LANG_Years = 'Years';
+$LANG_Months = 'Months';
+$LANG_Days = 'Days';
+
+$LANG_Body = 'Body';
+$LANG_Hair = 'Hair';
+$LANG_Beard = 'Beard';
+$LANG_Torso = 'Upper body';
+$LANG_Legs = 'Legs';
+$LANG_Hands = 'Hands';
+$LANG_Feet = 'Feet';
+$LANG_Body_shape = 'Body shape';
+$LANG_Shape_A = 'Shape A';
+$LANG_Shape_B = 'Shape B';
+
+$LANG_PayPal_Invalid_Input_Please_check_the_amount = "Invalid Input. Please check the amount.";
+$LANG_PayPal_Thank_you_for_your_purchase = "Thank you for your purchase!";
+$LANG_PayPal_Thank_you_for_your_donation = "Thank you for your donation!";
+
+$LANG_Edit_appearance = 'Edit appearance';
+$LANG_characters_allowed = 'characters allowed';
+$LANG_Invalid_image_URL = 'Invalid image URL';
+
+$LANG_You_drink = 'You drink';
+$LANG_Unknown_Skill = 'Unknown Skill';
+$LANG_Unknown_Ghoul = 'Unknown Ghoul';
+
+$LANG_Enter_your_name = 'Enter your name:';
+$LANG_Click_here_to_upload_avatar = 'Click here to upload avatar';
+$LANG_Click_here_to_upload_property = 'Click here to upload property';
+
+$LANG_has_been_added_to_your_inventory = 'has been added to your inventory!';
+$LANG_taken_off = 'taken off';
+$LANG_You_have_taken_off_a_piece_of_equipment = 'You have taken off a piece of equipment.';
+
+$LANG_Back_to_City = 'Back to City';
+$LANG_Back_to_Residential_Area = 'Back to Residential Area';
+
+$LANG_Your_name_cannot_be_longer_than_1 = 'Your name cannot be longer than';
+$LANG_Your_name_cannot_be_longer_than_2 = 'characters.';
+
+$LANG_Name_changed_to_1 = 'Name changed to:';
+$LANG_Name_changed_to_2 = '';
+
+$LANG_You_trained_hard = 'You trained hard';
+
+$LANG_Talk_to_people_passing_by = 'Talk to people passing by...';
+
+$LANG_BANK_WELCOME = "Welcome to City Bank. Deposit or withdraw your money for free to protect it while earning daily interest on your savings.";
+$LANG_BANK_INTEREST_INFO_1 = "Per day, the bank pays you";
+$LANG_BANK_INTEREST_INFO_2 = "% interest on your credit until you reach the upper limit of";
+$LANG_BANK_INTEREST_INFO_3 = ".";
+$LANG_BANK_LOAN_INTRO = "Good day! Do you need financial support for your plans? We offer attractive loans at favorable conditions.";
+$LANG_BANK_LOAN_INFO_1 = "With our loan you can make your dreams come true, be it a new home, a business or another investment. Our flexible repayment options and low interest rates make it easy for you to achieve your goals. Trust in our experience and let us find the best path for your financial future together. With your current score we can offer you a loan of up to";
+$LANG_BANK_LOAN_INFO_2 = " and in return you pay";
+$LANG_BANK_LOAN_INFO_3 = "interest on your remaining debts per day. You will not receive any interest on your balance while you are in debt.";
+$LANG_BANK_DEPT_INFO_1 = "You owe";
+$LANG_BANK_DEPT_INFO_2 = "to the bank. This means you are paying interest of";
+$LANG_BANK_DEPT_INFO_3 = "per Day.";
+
+$LANG_Your_Property = 'Your Property';
+$LANG_HOTEL_RENT_INTRO = 'Welcome to the hotel! Would you like to rent a room?';
+$LANG_HOTEL_RENT_INFO = 'The price for a room is';
+$LANG_Rent_Hotel_Room = 'Rent Hotel Room';
+$LANG_Rent_Now = 'Rent for 24 Hours';
+$LANG_You_rented_a_hotel_room = 'You rented a hotel room';
+$LANG_Your_Hotel_Room = 'Your Hotel Room';
+$LANG_You_are_save = 'You are save';
+$LANG_Room_No = 'Room No.';
+$LANG_Occupied = 'Occupied';
+$LANG_Other_rooms_in_hotel = 'Other rooms in hotel';
+$LANG_Property_of = 'Property of';
+$LANG_HOUSING_MARKLER_INTRO = "Hey, you there! Are you looking for a new home? I have an exclusive offer!";
+$LANG_HOUSING_MARKLER_SALES_PITCH_1 = "This house is not just a building, it is a safe haven in this troubled city. With its robust construction and strategic location, it offers you protection and comfort. Imagine how you can make your plans here in peace. You won't find an offer like this twice. For only";
+$LANG_HOUSING_MARKLER_SALES_PITCH_2 = "it's yours. What do you say?";
+$LANG_HOUSING_MARKLER_BUY_HOUSE = 'Buy the house for';
+$LANG_Buy_house = "Buy house";
+
+$LANG_HOUSING_LOCKED_DOOR = 'You are standing in front of a locked door, there is no way to get through.';
+$LANG_Talk_to_visitors_to_your_property = 'Talk to visitors to your property';
+$LANG_Insert_image_URL = 'Insert image URL';
+$LANG_This_is_not_a_valid_URL = 'This is not a valid URL.';
+$LANG_Wallpaper_replaced = 'Wallpaper replaced.';
+$LANG_Property_name_changed = 'Property name changed.';
+$LANG_Insert_label = 'Insert label';
+$LANG_Change_the_appearance_of_your_property = 'Change the appearance of your property';
+$LANG_Change_the_designation_that_appears_for_your_property = 'Change the designation that appears for your property.';
+$LANG_Add_wallpaper_to_your_hideout_by_saving_a_link_to_an_image_here = 'Add wallpaper to your hideout by saving a link to an image here.';
+
+$LANG_has_been_added_to_your_Ghouls = 'has been added to your Ghouls.';
+$LANG_You_have_reached_the_maximum_number_of_1 = 'You have reached the maximum number of';
+$LANG_You_have_reached_the_maximum_number_of_2 = '.';
+
+$LANG_Roam_the_city_have_adventures_and_discover_places = 'Roam the city, have adventures and discover new places!';
+
+$LANG_Key_received = 'Key received!';
+$LANG_Key_taken_back = 'Key taken back!';
+$LANG_gave_you_a_key_to_property = ' gave you a key to his/her property.';
+$LANG_has_taken_back_the_key_to_accomodation = ' has taken back the key to his/her property.';
+$LANG_You_gave_a_key_to_your_property_1 = 'You gave ';
+$LANG_You_gave_a_key_to_your_property_2 = ' a key to your property.';
+$LANG_You_took_the_key_to_your_property_from_1 = 'You took the key to your property from ';
+$LANG_You_took_the_key_to_your_property_from_2 = '.';
+
+$LANG_senses_are_particularly_heightened = '\'s senses are particularly heightened.';
+
+$LANG_Your_skill_has_increased_1 = 'Your skill ';
+$LANG_Your_skill_has_increased_2 = ' has increased.';
+
+$LANG_Wishing_Well = 'Wishing Well';
+$LANG_Throw_a_donation = 'Throw a donation into the fountain and help us close the city improve!';
+
+$LANG_What_a_bargain = 'What a bargain!';
+$LANG_You_dont_have_enough = 'You don\'t have enough';
+$LANG_You_bought_a_house = 'You bought a house!';
